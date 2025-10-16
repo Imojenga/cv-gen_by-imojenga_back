00a93ee3d09ac2b7911b0569e13d89b77026ee0c
Imojenga <aina.jenko@gmail.com>
2025-10-17 00:24:16 +0300

    fix task

--- src/controllers/cvs.js
+++ src/controllers/cvs.js
@@ -9,21 +9,75 @@ export const createCvController = async (req, res) => {
         properties: {},
         children: [
           new Paragraph({
+            alignment: 'center',
+            spacing: { after: 360 },
+            children: [new TextRun({ text: username, bold: true, size: 48 })],
+          }),
+          new Paragraph({
+            alignment: 'center',
+            spacing: { after: 240 },
+            children: [new TextRun({ text: position, bold: true, size: 32 })],
+          }),
+          new Paragraph({
+            alignment: 'center',
+            spacing: { after: 720 },
             children: [
-              new TextRun({ text: username, bold: true, size: 48 }),
-              new TextRun({ text: position, bold: true, size: 32 }),
-              new TextRun({ text: email }, { text: phone }, { text: city }),
+              new TextRun({ text: `${email}    ` }),
+              new TextRun({ text: `${phone}    ` }),
+              new TextRun({ text: city }),
             ],
           }),
-          exp
-            ? new Paragraph({ text: 'Work experience and skills:', bold: true })
-            : null,
-          exp ? new Paragraph(exp) : null,
-          edu ? new Paragraph({ text: 'Education:', bold: true }) : null,
-          edu ? new Paragraph(edu) : null,
-          add ? new Paragraph({ text: 'Additional info:', bold: true }) : null,
-          add ? new Paragraph(add) : null,
-        ],
+          exp &&
+            new Paragraph({
+              spacing: { after: 160 },
+              children: [
+                new TextRun({
+                  text: 'Work experience and skills:',
+                  bold: true,
+                  italics: true,
+                  size: 24,
+                }),
+              ],
+            }),
+          exp &&
+            new Paragraph({
+              spacing: { after: 560 },
+              children: [new TextRun({ text: exp, size: 22 })],
+            }),
+          edu &&
+            new Paragraph({
+              spacing: { after: 160 },
+              children: [
+                new TextRun({
+                  text: 'Education:',
+                  bold: true,
+                  italics: true,
+                  size: 24,
+                }),
+              ],
+            }),
+          edu &&
+            new Paragraph({
+              spacing: { after: 560 },
+              children: [new TextRun({ text: edu, size: 22 })],
+            }),
+          add &&
+            new Paragraph({
+              spacing: { after: 160 },
+              children: [
+                new TextRun({
+                  text: 'Additional info:',
+                  bold: true,
+                  italics: true,
+                  size: 24,
+                }),
+              ],
+            }),
+          add &&
+            new Paragraph({
+              children: [new TextRun({ text: add, size: 22 })],
+            }),
+        ].filter(Boolean),
       },
     ],
   });
--- src/validation/cvs.js
+++ src/validation/cvs.js
@@ -4,9 +4,9 @@ export const createCvSchema = Joi.object({
   username: Joi.string().min(3).max(50).required(),
   position: Joi.string().min(2).max(50).required(),
   email: Joi.string().required(),
-  phone: Joi.string().required(),
+  phone: Joi.number().required(),
   city: Joi.string().min(3).max(100).required(),
-  exp: Joi.string(),
-  edu: Joi.string(),
-  add: Joi.string(),
+  exp: Joi.string().allow(''),
+  edu: Joi.string().allow(''),
+  add: Joi.string().allow(''),
 });
